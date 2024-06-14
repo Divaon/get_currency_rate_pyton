bee6ae1ce0b08dab85c0ceeadc79acf6a51de09f
Divaon <56230474+Divaon@users.noreply.github.com>
2024-06-14 15:44:06 +0300

    Add new endpoint load_exchange_rate_by_code_on_date

--- controller.py
+++ controller.py
@@ -8,6 +8,14 @@ def exchange_rate_on_date():
     result = date
     return jsonify("Successful load exchange rate on date="+date)
 
+
+@app.route('/load_exchange_rate_by_code_on_date', methods=['POST'])
+def exchange_rate_by_code_on_date():
+    date = request.json.get('date')
+    currency_code = request.json.get('currency_code')
+    result = date
+    return jsonify("Successful load exchange rate on date="+date + " code="+currency_code)
+
 if __name__ == "__main__":
     app.run(
         host="127.0.0.1",
